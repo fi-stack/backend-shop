e40bccd7b757ec7bb2723b619c7432236b5d5fad
rafi-windows <rafi_n_o@github.com>
2020-12-11 16:00:03 +0700

    authentication laravel fortify

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace App\Http\Controllers\Admin;
+
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +16,20 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
+});
+
+/**
+ * route for admin
+ */
+
+//group route with prefix "admin"
+Route::prefix('admin')->group(function () {
+
+    //group route with middleware "auth"
+    Route::group(['middleware' => 'auth'], function () {
+
+        //route dashboard
+        Route::get('/dashboard', [DashboardController::class, 'index'])->name('admin.dashboard.index');
+    });
 });
