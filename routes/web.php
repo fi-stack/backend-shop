d1ea515e1560812409d6b8e090fe796bbbae14f6
rafi-windows <rafi_n_o@github.com>
2020-12-12 13:48:51 +0700

    crud users

--- routes/web.php
+++ routes/web.php
@@ -50,5 +50,8 @@ Route::prefix('admin')->group(function () {
 
         //profile
         Route::get('/profile', [ProfileController::class, 'index'])->name('admin.profile.index');
+
+        //route user
+        Route::resource('/user', UserController::class, ['except' => ['show'], 'as' => 'admin']);
     });
 });
